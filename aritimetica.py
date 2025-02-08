fb89b1cbdf550e4a2905327c1237d14b18bfa66e
gbrielbram18 <biel300816@gmail.com>
2025-02-08 12:03:24 -0300

    adicionando arquivo py

---
+++ aritimetica.py
@@ -0,0 +1,22 @@
+soma = 10 + 5 # 15
+print(soma)
+
+divisao = 10 / 3 # 3.33333333333
+print(divisao)
+
+mult = 10 * 5 # 50
+print(mult)
+
+subtrção= 10 - 5 # 5
+print(subtrção)
+
+resto = 10 % 3 # 1
+print(resto)
+
+exp= 10 ** 2 # 100
+print(exp)
+
+
+import math
+X = math.sqrt(64)
+print(X)
--- variaveis-aula1.py
+++ variaveis-aula1.py
@@ -5,3 +5,30 @@ idade = 37
  
 #string (texto) 
 nome = "gabriel"
+
+#boolean (verdadeiro ou falso)
+aprovado = True #T -> maiúsculo
+aprovado = False #F -> maiúsculo
+
+
+#list (lista)
+# estrutura de dados mutaveis e ordenada que pode contar 
+# diversos tipos dentro de si
+frutas = ["maçã", "uva", "laranja", 8 , False]
+
+#tuple (tupla)
+# estrutura de dados imutaveis (nao altera apos criar)
+cores = ("vermelho", "azul", "verde")
+
+# dict
+# estrutura de dados mutavel (chave valor)
+alumno= {
+    "nome": "gabriel",
+    "nota": 10
+}
+
+# set 
+# estrutura de dados mutavel, mas nao ordernada e nao indexada
+#armazena sem duplicar 
+conj_num = {1, 2, 3, } # 1, 2, 3
+
