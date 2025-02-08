857cd6431d81c8cc4c5244cbdbee277f4b3b0ebd
gbrielbram18 <biel300816@gmail.com>
2025-02-08 12:13:19 -0300

    adicionando condição-aula1

---
+++ condicao-aula1.py
@@ -0,0 +1,15 @@
+print(10==10) #True
+print(10!=5) #True
+print(10<5) #False
+print(10>5) #True
+
+
+#logico 
+
+print((10> 5) and (2 == 2 )) #true an true -> true
+print((10 < 3 ) and (2 == 2 )) #true an false -> false
+
+
+print((10 < 5) or (2 == 2 )) #false or true -> true
+print((10 > 5) or (2 == 3 )) #true or false -> true
+
