1390af0f7ac57f3ee0556ffcae20d02ccadc49e5
gbrielbram18 <biel300816@gmail.com>
2025-02-08 14:40:04 -0300

    adicionando exercicio2

--- exemplo_aula1.py
+++ exemplo_aula1.py
@@ -8,3 +8,7 @@ if idade >= 18:
     print("Você é maior de idade")
 else:
     print("Você é menor de idade")
+
+
+"""
+"""
---
+++ exer2.py
@@ -0,0 +1,8 @@
+# verificar se e impar ou par 
+
+"""
+Digite um numero inteiro
+verificar se e impar ou par
+escreva a mensagem correspondente 
+"""
+
