ad5f29d5c5de5502e7e405c7305cdebb27500fed
gbrielbram18 <biel300816@gmail.com>
2025-02-08 16:26:51 -0300

    adicionando exercicio 4

---
+++ exer4.py
@@ -0,0 +1,15 @@
+# conversor de temperatura
+
+"""
+solicite ao usuario para fornecer para fornecer uma temperatura em grau
+converta essa temperatura em fahrenheit
+"""
+
+print("ola seja bem vindo ao conversor de fahrenheit ")
+print("digite a temperatura em celcius")
+celcius = float(input())
+
+      
+fahrenheit = celcius * 1.8 + 32
+
+print("essa e a temperatura em fahrenheit: F", fahrenheit, )
