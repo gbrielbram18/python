fe13a7fdaa2116db36d5a690d5fe8e22e9392915
gbrielbram18 <biel300816@gmail.com>
2025-02-08 12:46:43 -0300

    adicionando atividade em py

--- condicao-aula1.py
+++ condicao-aula1.py
@@ -13,3 +13,4 @@ print((10 < 3 ) and (2 == 2 )) #true an false -> false
 print((10 < 5) or (2 == 2 )) #false or true -> true
 print((10 > 5) or (2 == 3 )) #true or false -> true
 
+print(not( 10 == 10)) #not true -> false
---
+++ exerc1.py
@@ -0,0 +1,19 @@
+#soma de 3 numeros
+
+print ("seja bem vindo ao inseridor de 3 numeros")
+print ("digita um  numero")
+n1= int(input ())
+
+
+print ("digita outro numero")
+n2= int(input ())
+
+print ("digite o teceiro numero")
+n3= int(input ()) #ou usar float
+
+soma = n1 + n2 + n3 
+print(" a soma e: " ,soma)
+
+resultado = n1 + n2 + n3
+
+print ("esse e seu resultado: " , resultado)
