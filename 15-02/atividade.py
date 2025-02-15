1848c68fc230961feb60a8676bd4319f13449a0b
gbrielbram18 <biel300816@gmail.com>
2025-02-15 10:41:52 -0300

    exercicio

---
+++ 15-02/atividade.py
@@ -0,0 +1,13 @@
+#cinema
+#gasparxinho 3 jkkkkkk
+idade = 18
+
+if idade >= 18:
+    print("pode entar no cinema")
+
+elif idade > 50 :
+    print("procure outro filme")
+
+elif idade < 18 : 
+    print ("nao pode entrar")
+    
