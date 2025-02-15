ece5ce96e14ca0a2954b658aa3396fd8df1e830f
gbrielbram18 <biel300816@gmail.com>
2025-02-15 09:58:39 -0300

    adicionando arquivos em python

--- 15-02/revisão.py
+++ 15-02/revisão.py
@@ -5,7 +5,17 @@
 nome = "gabriel"
 
 #inteiros(numeros)
-idade = 18
+idade = 18  
 
 #booleano
 adulto = False
+
+#float
+saldo = 3.14
+negativos = -2.5
+
+#listas em python
+listas = [True, "texto", 10, -50]
+
+#vetor
+#adicionar, editar, remover
