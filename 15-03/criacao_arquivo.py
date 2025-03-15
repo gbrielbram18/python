3b715d2a5dbd0cd30f9fceea2da02c185abd4575
gbrielbram18 <biel300816@gmail.com>
2025-03-15 15:40:35 -0300

    adicionando arquivo ligado com exel

--- 15-03/criacao_arquivo.py
+++ 15-03/criacao_arquivo.py
@@ -23,3 +23,17 @@ dados = [
     ["roger Gedes","26","Arabia saldita"]
     ["eren","18","paradis"]
 ]
+
+
+#Criar arquivo cvs
+with open("dados.csv", "w", newline="", encoding="utf-8") as csvarquivo:
+    escritor = csv.writer(csvarquivo)
+    escritor.writerow(dados)
+    
+    
+#Ler arquivo csv
+with open ("dados.csv", "r" , encoding="utf-8") as csvarquivo:
+    leitor = csv.reader(csvarquivo)
+    print("\nConteudo do arquivo csv")
+    for linha in leitor:
+        print(linha)
---
+++ 15-03/panda_csv.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+#criar os dados para o nosso dataframe
+
+dados = {
+    "nome" : ["Arthur","Maria", "Matheus","Carlos", "Bruna"],
+    "Idade" : [28,22,18,35,20],
+    "Cidade" : ["Cotia","Carapikuiba","Cotia","Osasco","Cotia"]
+}
+
+df = pd.DataFrame(dados)
+#Exibir o Dataframe
+print(df)
+
+#salvar dataFrame
+df.to_csv("panda_dados.csv" , index= False)
+
+#Visualizar em data frame CSV
+df_csv = pd.read_csv("panda_dados.csv")
+
+df_fitrar = df[df["Idade"] > 25]
+print (df_fitrar) #Todas as pessoas com menos de 25 anos não aparecem
+
+        
